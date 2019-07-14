4f6315e664ec0ef827e1dd5ddf59aaeb07a8e4d8
cpsnell <cpsnell@gmail.com>
2019-07-14 11:34:01 -0400

    Add files via upload

---
+++ XxxTest.cpp
@@ -0,0 +1,8 @@
+#include "TestHarness.h"
+
+// Rename this file to match the functionality under test. E.g., StringTest.
+// Add tests and CHECKs as required
+TEST(char, strings)
+{
+    CHECK_EQUAL(1, 1);
+}
